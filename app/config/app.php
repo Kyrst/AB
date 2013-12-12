fddc0780e4354555492b4289ed65f798999038e5
Dennis <nygganh@nygganh.se>
2013-12-12 13:29:59 -0800

    Fix.

--- app/config/app.php
+++ app/config/app.php
@@ -114,9 +114,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 
-		'Toddish\Verify\VerifyServiceProvider',
-
-		'Kyrst\Base\BaseServiceProvider'
+		'Toddish\Verify\VerifyServiceProvider'
 	),
 
 	/*
