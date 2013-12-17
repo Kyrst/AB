9860893d7e8c7946d65b517058365dc8316d4ada
Dennis <nygganh@nygganh.se>
2013-12-16 16:14:08 -0800

    E-mail font fix.

--- app/views/emails/welcome_beta.php
+++ app/views/emails/welcome_beta.php
@@ -15,13 +15,13 @@
 						<h2 style="font:normal 15px Arial;color:#3D3D3D">THANK YOU FOR SIGNING UP FOR ACTINGBIO</h2>
 
 						<div style="margin-top:35px;text-align:left;padding:0 36px">
-							<p style="color:#84949F;font-size:15px;line-height:32px">
+							<p style="color:#84949F;font-size:15px;line-height:32px;font-family:Times new roman">
 								ActingBio is still in BETA (in its early stages of development), which means that as a thank you for being an early adopter, we’ve assigned you a unique ID <span style="color:#A37E2C;font-weight:bold">#<?= $code ?></span> for you to set up your account before anyone else.
 								<br>
 								Once we release the full version you’ll get access to try out new features, insight acting information, promo codes as well as discounts on all of our pricing plans.
 							</p>
 
-							<p style="color:#586670;font:bold 14px Arial;margin-top:25px">
+							<p style="color:#586670;font:bold 15px Arial;margin-top:25px">
 								Your ActingBio ID: <span style="color:#A37E2C">#<?= $code ?></span>
 							</p>
 						</div>
