8ba15a3a1893498102b952cdacb02635b2c7f8ea
Gaston <gaston@actingbio.com>
2014-01-07 21:01:04 -0800

    simple changes to the p tags ~G

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -5,10 +5,10 @@ return array
 	(
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => '127.0.0.1',
+			'host'      => 'localhost',
 			'database'  => 'actingbio',
 			'username'  => 'root',
-			'password'  => 'p3edz9',
+			'password'  => 'root',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => ''
